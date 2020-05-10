414289d1b939762748b000d432cf0168cb789deb
Plane <hordii039@gmail.com>
2020-05-10 13:04:02 +0300

    fh

--- src/com/company/ArrayList.java
+++ src/com/company/ArrayList.java
@@ -1,2 +1,46 @@
+package com.company;
 
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+public class ArrayList{
+    int IndexOfList = 10;
+    Integer[] arrayList = new Integer[IndexOfList];
+    public void get(int index) {
+        System.out.println(arrayList[index]);
+    }
+    public void add(int num, int index) {
+        if (arrayList[index] != null) {
+            System.out.println("index is invalid");
+            System.exit(0);
+        }
+//        if (arrayList[arrayList.length - 1] != null) {
+//            Integer[]arrayLists = Arrays.copyOf(arrayList, 100);
+//            arrayLists[index] = num;
+//            System.out.println(arrayLists[index]);
+//        }
+        arrayList[index] = num;
+        System.out.println(arrayList[index]);
+
+    }
+    public void size() {
+        System.out.println(IndexOfList);
+    }
+    public void remove(int index) {
+        arrayList[index] = null;
+        System.out.println(arrayList[index]);
+    }
+    public void clear() {
+        for (int i = 0; i <arrayList.length - 1 ; i++) {
+            arrayList[i] = null;
+            System.out.println(arrayList[i]);
+        }
+    }
+    public void set(int index, int num) {
+        arrayList[index] = num;
+        System.out.println(arrayList[index]);
+    }
+
+}
 
---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+public class Main{
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(0, 0);
+        arrayList.add(1, 1);
+        arrayList.add(2, 2);
+        arrayList.add(3, 3);
+        arrayList.add(4, 4);
+        arrayList.add(5, 5);
+        arrayList.add(6, 6);
+        arrayList.add(7,7);
+        arrayList.add(8,8);
+        arrayList.add(9,9);
+//        arrayList.add(10,10);
+//        arrayList.add(11,11);
+//        arrayList.add(12,12);
+
+
+    }
+}
