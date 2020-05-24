d730f5d01af87cc35defc7db64c05fa4bc2c79b2
Plane <hordii039@gmail.com>
2020-05-24 11:25:23 +0300

    fh

--- src/com/company/ArrayList.java
+++ src/com/company/ArrayList.java
@@ -1,45 +1,58 @@
 package com.company;
 
-import java.io.InputStream;
 import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.List;
-public class ArrayList{
+
+public class ArrayList<T> {
     int IndexOfList = 10;
-    Integer[] arrayList = new Integer[IndexOfList];
-    public void get(int index) {
-        System.out.println(arrayList[index]);
+    T[] arrayList = (T[]) new Object[IndexOfList];
+    int ScoreOfSize = 0;
+
+    public int newSize() {
+        return IndexOfList *= 1.5;
     }
-    public void add(int num, int index) {
+
+    void add(T el) {
+        ScoreOfSize++;
+        if (ScoreOfSize >= IndexOfList) {
+            IndexOfList *= 1.5;
+            T[] newArray = (T[]) new Object[IndexOfList];
+            Arrays.copyOf(arrayList, ScoreOfSize);
+            arrayList = newArray;
+        }
+        arrayList[ScoreOfSize] = el;
+        System.out.println(arrayList[ScoreOfSize]);
+
+    }
+
+    public T get(int index) {
+        return arrayList[index];
+    }
+
+    public int size() {
+        return arrayList.length;
+    }
+
+    public void remove(int index) {
+        ScoreOfSize--;
+        arrayList[index] = null;
+    }
+
+    public void clear() {
+        for (int i = 0; i < arrayList.length - 1; i++) {
+            arrayList[i] = null;
+            System.out.println(arrayList[i]);
+        }
+        ScoreOfSize = 0;
+    }
+
+    public T set(int index, T el) {
         if (arrayList[index] != null) {
             System.out.println("index is invalid");
             System.exit(0);
         }
-//        if (arrayList[arrayList.length - 1] != null) {
-//            Integer[]arrayLists = Arrays.copyOf(arrayList, 100);
-//            arrayLists[index] = num;
-//            System.out.println(arrayLists[index]);
-//        }
-        arrayList[index] = num;
-        System.out.println(arrayList[index]);
-
-    }
-    public void size() {
-        System.out.println(IndexOfList);
-    }
-    public void remove(int index) {
-        arrayList[index] = null;
-        System.out.println(arrayList[index]);
-    }
-    public void clear() {
-        for (int i = 0; i <arrayList.length - 1 ; i++) {
-            arrayList[i] = null;
-            System.out.println(arrayList[i]);
-        }
-    }
-    public void set(int index, int num) {
-        arrayList[index] = num;
-        System.out.println(arrayList[index]);
+        arrayList[index] = el;
+        return arrayList[index];
     }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,27 @@
 package com.company;
 public class Main{
-    public static void main(String[] args) {
-        ArrayList arrayList = new ArrayList();
-        arrayList.add(0, 0);
-        arrayList.add(1, 1);
-        arrayList.add(2, 2);
-        arrayList.add(3, 3);
-        arrayList.add(4, 4);
-        arrayList.add(5, 5);
-        arrayList.add(6, 6);
-        arrayList.add(7,7);
-        arrayList.add(8,8);
-        arrayList.add(9,9);
-//        arrayList.add(10,10);
-//        arrayList.add(11,11);
-//        arrayList.add(12,12);
+
+
+
+
+        public static void main(String[] args) {
+ArrayList arrayList = new ArrayList();
+
+        arrayList.add("ciu");
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(4);
+        arrayList.add(5);
+        arrayList.add(6);
+        arrayList.add(7);
+        arrayList.add(8);
+        arrayList.add(9);
+        arrayList.add(10);
+        arrayList.add(11);
+        arrayList.add(12);
+                arrayList.remove(5);
+
 
 
     }
